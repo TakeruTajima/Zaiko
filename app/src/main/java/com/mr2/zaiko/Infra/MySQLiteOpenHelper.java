833360b0a06ba4fdd147dc569050b872e3043bc6
Takeru Tajima <mr2o527@gmail.com>
2019-12-03 03:03:29 +0900

    table設計変更

--- app/src/main/java/com/mr2/zaiko/Infra/MySQLiteOpenHelper.java
+++ app/src/main/java/com/mr2/zaiko/Infra/MySQLiteOpenHelper.java
@@ -29,7 +29,7 @@ import androidx.annotation.NonNull;
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     public static final String TAG = MySQLiteOpenHelper.class.getSimpleName();
     private static final String DB_NAME = "test.db";
-    private static final int DB_VERSION = 3;
+    private static final int DB_VERSION = 4;
 
     MySQLiteOpenHelper(@NonNull Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -39,7 +39,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         Log.d(TAG, "onCreateView");
-        createTableVer2(db);
+        createTableVer4(db);
     }
 
     @Override
@@ -70,8 +70,9 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
                 addTableVer3(db);
             case 2:
                 addTableVer3(db);
-
             case 3:
+                dropTableVer4(db);
+                createTableVer4(db);
             case 4:
             case 5:
             case 6:
@@ -92,6 +93,47 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 //                Log.d(TAG, "Upgrade failed, unknown newVersion. new ver." + newVersion);
 //        }
 //        break;
+    }
+
+    private void dropTableVer4(SQLiteDatabase db){
+        Log.d(TAG, "dropTableVer1");
+        db.execSQL("DROP TABLE m_unit_types;");
+        db.execSQL("DROP TABLE m_companies;");
+        db.execSQL("DROP TABLE m_items;");
+        db.execSQL("DROP TABLE m_images");
+    }
+
+    private void createTableVer4(SQLiteDatabase db){
+        Log.d(TAG, "createTableVer4");
+        db.execSQL("CREATE TABLE `m_unit_types` ( " +
+                "`_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "`name` TEXT, " +
+                "`created_at` TEXT default current_timestamp, " +
+                "`deleted_at` TEXT )");
+        db.execSQL("CREATE TABLE `m_companies` ( " +
+                "`_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "`name` TEXT, " +
+                "`is_maker` INTEGER, " +
+                "`is_seller` INTEGER, " +
+                "`created_at` TEXT default current_timestamp, " +
+                "`deleted_at` TEXT )");
+        db.execSQL("CREATE TABLE `m_items` ( " +
+                "`_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "`model` TEXT, " +
+                "`name` TEXT, " +
+                "`primary_image_id` TEXT, " +
+                "`company_id` INTEGER, " +
+                "`in_house_code` INTEGER, " +
+                "`unit_type_id` INTEGER, " +
+                "`value` INTEGER, " +
+                "`take_stock` INTEGER, " +
+                "`created_at` TEXT default current_timestamp, " +
+                "`deleted_at` TEXT )");
+        db.execSQL("CREATE TABLE `m_images` ( " +
+                "`_id` INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "`item_id` INTEGER NOT NULL, " +
+                "`address` TEXT NOT NULL UNIQUE, " +
+                "`created_at` TEXT DEFAULT current_timestamp )");
     }
 
     private void addTableVer3(SQLiteDatabase db){
